452572ac16755d1ffee857e6549b8d01c4a8eda6
Sai Harshith <234g1a05d5@srit.ac.in>
2026-07-20 11:24:09 +0530

    Time: 171 ms (28.83%) | Memory: 93 MB (86.83%) - LeetSync

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        for (int num : nums) {
+            pq.offer((long) num);
+        }
+
+        int operations = 0;
+
+        while (pq.size() > 1 && pq.peek() < k) {
+
+            long x = pq.poll();
+            long y = pq.poll();
+
+            long newValue = x * 2 + y;
+
+            pq.offer(newValue);
+
+            operations++;
+        }
+
+        return operations;
+    }
+}
